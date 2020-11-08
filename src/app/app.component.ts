6f19a22691cec9e93dae5010a781d2df8c6f2f9b
Maxime Cansell <maxime.cansell@gmail.com>
2020-11-08 18:43:26 +0100

    troisieme commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit} from '@angular/core';
+import {POKEMONS} from './mock-pokemons';
+import {Pokemon} from './pokemon';
+
+
+@Component({
+  selector: 'app-root',
+  template: `<h1> Hello, {{name}}</h1>`,
+  styles: []
+})
+export class AppComponent implements OnInit {
+  name = 'Maxime';
+  pokemons: Pokemon[] = null;
+
+  ngOnInit(){
+    this.pokemons = POKEMONS;
+  }
+
+  selectPokemon(pokemon: Pokemon){
+    console.log('Vous avez séléctionné ' + pokemon.name);
+
+  }
+}
