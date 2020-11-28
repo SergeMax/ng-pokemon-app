4feeb6aaf06d80c18ac02d5e8419e34ad9288635
Maxime Cansell <maxime.cansell@gmail.com>
2020-11-28 18:36:30 +0100

    commit add --all

---
+++ src/app/list-pokemon.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit} from '@angular/core';
+import {POKEMONS} from './mock-pokemons';
+import {Pokemon} from './pokemon';
+import {Router} from '@angular/router';
+
+
+@Component({
+  selector: 'list-pokemon',
+  templateUrl: './list-pokemon.component.html',
+  styles: []
+})
+export class ListPokemonComponent implements OnInit {
+  name = 'Maxime';
+  pokemons: Pokemon[] = null;
+
+  constructor(private router: Router){
+
+  }
+
+  ngOnInit(){
+    this.pokemons = POKEMONS;
+  }
+
+  selectPokemon(pokemon: Pokemon): void {
+
+    let link = ['/pokemon', pokemon.id];
+    this.router.navigate(link);
+  }
+}
